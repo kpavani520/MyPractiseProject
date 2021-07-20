84eada13653046a120ca29e2606cf9c2831ac5da
kpavani520 <kpavani520@gmail.com>
2021-07-20 13:55:15 -0700

    gitExercise file is modified

--- src/gitExercise1/example1.java
+++ src/gitExercise1/example1.java
@@ -5,6 +5,9 @@ public class example1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My first git learning");
+		System.out.println("My second git learning");
+		System.out.println("My third git learning");
+		
 
 	}
 
